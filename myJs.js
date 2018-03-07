23f87042339edd931b778b99687c496f1f42c62a
Brian Floyd <brianfloyd@Brians-Mac.local>
2018-03-07 16:55:02 -0600

    All files

---
+++ myJs.js
@@ -0,0 +1,36 @@
+$(document).ready(function(){
+    
+  
+var ex = ( function(){
+
+
+    return ({
+
+        fun1:function(){console.log('1')},
+        fun2:function(){console.log('2')},
+        fun3:function(){console.log('3')},
+        fun4:function(){console.log('4')}
+
+
+
+
+    }
+
+
+
+    )
+})
+
+
+
+
+ex().fun1()
+
+       
+
+  
+
+
+});
+
+
